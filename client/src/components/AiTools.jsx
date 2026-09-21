a731bb6ccd7f3091b0944116bfac9260906a8577
Ritwik Chandra <ritwikchandra8@gmail.com>
2026-09-21 15:50:23 +0530

    chore: checkpoint before Nexora AI rebuild

--- client/src/components/AiTools.jsx
+++ client/src/components/AiTools.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import { AiToolsData } from '../assets/assets'
-
+import { useNavigate } from 'react-router-dom'
+import { useUser } from '@clerk/react'
 const AiTools = () => {
+
+  const navigate = useNavigate()
+  const {user} = useUser()
+
   return (
     <section className='w-full px-4 sm:px-20 xl:px-32 -mt-40 pt-0 pb-24'>
       <div className='w-full flex flex-col items-center text-center mx-auto'>
@@ -17,7 +22,11 @@ const AiTools = () => {
       <div className='flex flex-wrap mt-10 justify-center '>
         {AiToolsData.map((tool, index) =>(
           <div key={index} className=' p-8 m-4 max-w-xs rounded-lg bg-[#FDFDFE] shadow-1g border border-gray-100 hover:-translate-y-1
-           transition-all duration-300 cursor-pointer'> </div>
+           transition-all duration-300 cursor-pointer' onClick={()=>user && navigate(tool.path)}>
+             <tool.Icon className='w-12 h-12 p-3 text-white rounded-xl' style={{ background: `linear-gradient(to bottom, ${tool.bg.from}, ${tool.bg.to})`}}/>
+             <h3 className='mt-6 mb-3 text-lg font-semibold'> {tool.title} </h3>
+             <p className='text-gray-400 text-sm max-w-[95%]'> {tool.description} </p>
+             </div>
         ) )}
 
       </div>
